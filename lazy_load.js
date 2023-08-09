a369bb52b9f842a03406424be9defa6482822a6f
h <h>
2023-08-09 18:04:30 +0700

    g

--- lazy_load.js
+++ lazy_load.js
@@ -9,7 +9,23 @@ function openEventDetailPage(eventId) {
       window.location.href = `event_detail.html?id=${eventId}`;
     }
     
+const searchInput = document.getElementById("searchInput");
+const dataItems = document.querySelectorAll(".data-item");
+
+searchInput.addEventListener("input", function() {
+  const searchQuery = searchInput.value.toLowerCase();
+  
+  dataItems.forEach(item => {
+    const itemText = item.textContent.toLowerCase();
     
+    if (itemText.includes(searchQuery)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
+
     
     
     
