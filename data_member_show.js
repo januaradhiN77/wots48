0b652712aef534540e54f23362f95112a14b8b6a
g <g>
2023-08-23 18:39:15 +0700

    b

--- data_member_show.js
+++ data_member_show.js
@@ -213,6 +213,57 @@ members: [
         ]
       
             },
+        {
+        id: 9,
+        title: "Aturan Anti Cinta",
+        date: "19:00, Kamis 24 Agustus 2023",
+        icon: "https://jkt48webfans.netlify.app/icon.cat17_jkt48.png",
+        image: "https://jkt48webfans.netlify.app/poster.aturananticinta.jpg",
+        informasiLink: "https://jkt48.com/theater/schedule/id/2580?lang=id",
+                members: [
+          { name: "Ashel", link: "ashel.html", photo: "https://jkt48.com/profile/adzana_shaliha.jpg?v=20230116", status: "Anggota JKT48"},
+          { name: "Callie", link: "callie.html", photo: "https://jkt48.com/profile/callista_alifia.jpg?v=20230116", status: "Anggota JKT48" },
+          { name: "Cathy", link: "cathy.html", photo: "https://jkt48.com/profile/cathleen_nixie.jpg?v=20230116", status: "Trainee JKT48" },
+          { name: "Chelsea", link: "chelsea.html", photo: "https://jkt48.com/profile/chelsea_davina.jpg?v=20230116", status: "Trainee JKT48"},
+                    { name: "Olla",  link: "olla.html", photo: "https://jkt48.com/profile/febriola_sinambela.jpg?v=20230116", status: "Anggota JKT48" },
+          { name: "Fiony",  link: "fiony.html", photo: "https://jkt48.com/profile/fiony_alveria.jpg?v=20230116", status: "Anggota JKT48" },
+          { name: "Freya", link: "freya.html", photo: "https://jkt48.com/profile/freya_jayawardana.jpg?v=20230116", status: "Anggota JKT48" },
+          { name: "Eli", link: "eli.html", photo: "https://jkt48.com/profile/helisma_putri.jpg?v=20230116" , status: "Anggota JKT48"},
+                              { name: "Indah",  link: "indah.html", photo: "https://jkt48.com/profile/indah_cahya.jpg?v=20230116", status: "Anggota JKT48" },
+          { name: "Lyn", link: "lyn.html", photo: "https://jkt48.com/profile/jesslyn_elly.jpg?v=20230116" , status: "Anggota JKT48"},
+          { name: "Kathrina", link: "kathrina.html", photo: "https://jkt48.com/profile/kathrina_irene.jpg?v=20230116" , status: "Anggota JKT48"},
+          { name: "Lulu", link: "lulu.html", photo: "https://jkt48.com/profile/lulu_salsabila.jpg?v=20230116" , status: "Anggota JKT48"},
+          { name: "Marsha", link: "marsha.html", photo: "https://jkt48.com/profile/marsha_lenathea.jpg?v=20230116", status: "Anggota JKT48" },
+          { name: "Muthe", link: "muthe.html", photo: "https://jkt48.com/profile/mutiara_azzahra.jpg?v=20230116", status: "Anggota JKT48" },
+          { name: "Raisha", link: "raisha.html", photo: "https://jkt48.com/profile/raisha_syifa.jpg?v=20230116", status: "Anggota JKT48" },
+          { name: "Chika", link: "chika.html", photo: "https://jkt48.com/profile/yessica_tamara.jpg?v=20230116", status: "Anggota JKT48" },
+        ]
+        
+      },
+                  {  // Belum Dipakai //
+                    id: 10,
+                    title: "Ingin Bertemu",
+                    date: "19:00, Jumat 25 Agustus 2023",
+                    icon: "https://jkt48webfans.netlify.app/icon.cat17_jkt48.png",
+                    image: "https://jkt48webfans.netlify.app/poster.jkt48.jpg",
+                    informasiLink: "https://jkt48.com/theater/schedule/id/2581?lang=id",
+members: [
+          { name: "Alya", link: "alya.html", photo: "https://jkt48.com/profile/alya_amanda.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Anindya", link: "anindya.html", photo: "https://jkt48.com/profile/anindya_ramadhani.jpg?v=20230116", status: "Trainee JKT48" },
+          { name: "Cathy", link: "cathy.html", photo: "https://jkt48.com/profile/cathleen_nixie.jpg?v=20230116", status: "Trainee JKT48" },
+          { name: "Chelsea", link: "chelsea.html", photo: "https://jkt48.com/profile/chelsea_davina.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Cynthia", link: "cynthia.html", photo: "https://jkt48.com/profile/cynthia_yaputera.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Danella", link: "danella.html", photo: "https://jkt48.com/profile/dena_natalia.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Daisy", link: "daisy.html", photo: "https://jkt48.com/profile/desy_natalia.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Gendis", link: "gendis.html", photo: "https://jkt48.com/profile/gendis_mayrannisa.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Gracie", link: "gracie.html", photo: "https://jkt48.com/profile/grace_octaviani.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Greesel", link: "greesel.html", photo: "https://jkt48.com/profile/greesella_adhalia.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Jeane", link: "jeane.html", photo: "https://jkt48.com/profile/jeane_victoria.jpg?v=20230116", status: "Trainee JKT48"  },
+          { name: "Michie", link: "michie.html", photo: "https://jkt48.com/profile/michelle_alexandra.jpg?v=20230116", status: "Trainee JKT48"  },
+
+        ]
+      
+            },
     ];
 function redirectToExternalLink(link) {
       window.location.href = (link);
