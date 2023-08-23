190567b1b875512e8f58d46fc921eb978fcacfe4
v <v>
2023-08-23 18:22:26 +0700

    v

--- css_js/data_icon.js
+++ css_js/data_icon.js
@@ -13,4 +13,5 @@ addImageToElement('eventicon', 'https://jkt48.com/images/icon.cat2.png');
 addImageToElement('goodsicon', 'https://jkt48.com/images/icon.cat6.png');
 addImageToElement('bdayicon', 'https://jkt48.com/images/icon.cat5.png');
 addImageToElement('othericon', 'https://jkt48.com/images/icon.cat8.png');
+addImageToElement('releaseicon', 'https://jkt48.com/images/icon.cat4.png');
 addImageToElement('jkt48icon', 'https://jkt48webfans.netlify.app/icon.cat17_jkt48.png');
