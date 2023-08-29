0bfecfb0712544cd7762f6740e7fcd552da9429d
g <g>
2023-08-29 18:16:21 +0700

    y

--- date_time.js
+++ date_time.js
@@ -1,1 +1,24 @@
+// Ambil elemen-elemen yang ingin kamu atur tampilannya
+var jadwalElements = document.querySelectorAll(".jadwal_date"); // Ganti ".jadwal_element" dengan selektor yang sesuai
 
+// Tentukan tanggal dan waktu target
+var targetDate = new Date("2023-09-01T00:00:00"); // Ganti dengan tanggal dan waktu yang sesuai
+
+// Fungsi untuk memeriksa dan ubah tampilan untuk setiap elemen
+function updateDisplay() {
+  var currentDate = new Date();
+
+  jadwalElements.forEach(function(jadwalElement) {
+    if (currentDate < targetDate) {
+      jadwalElement.style.display = "none";
+    } else {
+      jadwalElement.style.display = "block";
+    }
+  });
+}
+
+// Panggil fungsi updateDisplay secara berkala
+setInterval(updateDisplay, 1000); // Cek setiap detik
+
+// Panggil updateDisplay saat halaman dimuat
+updateDisplay();
