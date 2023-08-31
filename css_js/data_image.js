8d8b567d8f3aa28d26b9c12fb1717c50594af62d
g <g>
2023-08-31 23:01:54 +0700

    g

--- css_js/data_image.js
+++ css_js/data_image.js
@@ -56,7 +56,9 @@ addImageToElement('michieimg', 'https://jkt48.com/profile/michelle_alexandra.jpg
 //IDN Thumbnail
 
 
-addImageToElement('idnapp', 'https://pbs.twimg.com/media/F4nhE1uasAAWdvN?format=jpg&name=medium');
+addImageToElement('idnapp', '6066066066');
+
+
 
 //SHOWROOM IMAGE MEMBER :)
 
@@ -74,10 +76,15 @@ addImageToElement('liasr', 'https://static.showroom-live.com/image/room/cover/10
 
 
 addImageToElement('gitasr', 'https://static.showroom-live.com/image/room/cover/388fa55ee4ee2a438a34d1b0a71de03f472b76b5560203930f103850edb577c8_s.jpeg?v=1675092700');
+
 addImageToElement('fionysr', 'https://static.showroom-live.com/image/room/cover/a2345e2127cf122ef7adb9d988a41e080dbd7f0db28bfc113335b7a341b68c7e_s.jpeg?v=1675092072');
+
 addImageToElement('indahsr', 'https://static.showroom-live.com/image/room/cover/58f3d939319e28956fd771e0f587a347ea0fec6b5c3415067e122f4794fd3514_s.jpeg?v=1675091960');
+
 addImageToElement('gendissr', 'https://static.showroom-live.com/image/room/cover/4e2f064db08d9b22d45a2ff98271ca8754e2f058981823193d38c8e22cb09e00_s.jpeg?v=1683720244');
+
 addImageToElement('ollasr', 'https://static.showroom-live.com/image/room/cover/79b9c42d1f5ea5d5805fefcfac6e92198e434cd6b6244c79b309b9e93ab3f7cb_s.jpeg?v=1675092110');
+
 addImageToElement('michiesr', 'https://static.showroom-live.com/image/room/cover/fc878be06fb484b4edcb8051e8456a93279b2395e711aada3d7547b1b78e3720_s.jpeg?v=1683720193');
 
 
