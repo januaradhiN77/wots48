0044208e3c483ab9badf66ed2270d003c3755432
j <j>
2023-09-19 05:58:18 +0700

    h

--- css_js/button_data.js
+++ css_js/button_data.js
@@ -1,5 +1,5 @@
 
     function goToWebsite(websiteURL) {
-      window.location.href = websiteURL;
-      //alert("Oops! Terjadi kesalahan. 4000");
+      //window.location.href = websiteURL;
+      alert("Oops! Terjadi kesalahan. 4000");
     }
