11e3eeed0efd9b1660067997dd4477f11e15278b
j <j>
2023-09-22 20:24:07 +0700

    j

--- error_message.js
+++ error_message.js
@@ -4,13 +4,13 @@ function showError() {
   
   
   // Munculkan pesan kesalahan dengan animasi
-  errorContainer.style.display = 'block';
+  errorContainer.style.display = 'none';
   
   setTimeout(() => {
     errorContainer.style.transform = 'translate(-50%, -50%) scale(1)';
   }, 100);
   
-  background.style.display = 'block';
+  background.style.display = 'none';
   
 
 }
@@ -31,21 +31,3 @@ function hideError() {
 
 setInterval(showError, 2000);
 
-function noneD(){
-const merchandise = document.getElementById('merch');
-const schedule = document.getElementById('sc');
-const mmbr = document.getElementById('anggota');
-const berita = document.getElementById('brt');
-const birthday = document.getElementById('bday');
-const menuBar = document.getElementById('menu')
-
-merchandise.style.display = 'none';
-  schedule.style.display = 'none';
-  mmbr.style.display = 'none';
-  berita.style.display = 'none';
-  birthday.style.display = 'none';
-  menuBar.style.display = 'none';
-  
-}
-
-noneD();
