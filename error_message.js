0af4f25141ee9ad5babf0060cdd3f2b08fc79b62
j <j>
2023-09-22 18:53:02 +0700

    j

---
+++ error_message.js
@@ -0,0 +1,26 @@
+function showError() {
+  const errorContainer = document.getElementById('errorContainer');
+  const background = document.getElementById('background');
+  
+  // Munculkan pesan kesalahan dengan animasi
+  errorContainer.style.display = 'block';
+  setTimeout(() => {
+    errorContainer.style.transform = 'translate(-50%, -50%) scale(1)';
+  }, 100);
+  
+  background.style.display = 'block';
+}
+
+function hideError() {
+  const errorContainer = document.getElementById('errorContainer');
+  const background = document.getElementById('background');
+  
+  // Sembunyikan pesan kesalahan dengan animasi
+  errorContainer.style.transform = 'translate(-50%, -50%) scale(0)';
+  setTimeout(() => {
+    errorContainer.style.display = 'none';
+    background.style.display = 'none';
+  }, 300);
+}
+
+setInterval(showError, 5000);
