99d4f8763cd4065487f866b21b9a68ebb5a7491a
adhinugroho623@gmail.com <adhinugroho623@gmail.com>
2023-11-14 16:06:25 +0700

    update data_idnlive.js

--- data_idnlive.js
+++ data_idnlive.js
@@ -7,7 +7,7 @@ var idnliveId = urlParams.get('idnlive');
  if (idnliveId === 'idnapp') {
     document.getElementById('namaMember').innerHTML = 'Temen Main JKT48';document.getElementById('memberSt').innerHTML = 'IDN Live';
     document.getElementById('status').innerHTML = '• 15:32';
-    document.getElementById('iframeLive').src = 'https://www.api.riizeis.me/?url_hash=https://4b964ca68cf1.us-east-1.playback.live-video.net/api/video/v1/us-east-1.050891932989.channel.4idUspKIZskG.m3u8';
+    document.getElementById('iframeLive').src = 'https://4b964ca68cf1.us-east-1.playback.live-video.net/api/video/v1/us-east-1.050891932989.channel.4idUspKIZskG.m3u8';
     
     
 }
